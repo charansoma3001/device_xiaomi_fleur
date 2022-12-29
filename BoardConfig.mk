a72331493c53c217ebd5298d3ad1341318b4b5dd
aghora7 <ukikieran@gmail.com>
2022-12-29 15:53:26 +0530

    fleur: drop device,framework manifest
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,8 +98,6 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
