f00e60666bc833d6d32c8d942528ac2c98aef011
aghora7 <ukikieran@gmail.com>
2023-01-05 22:35:25 +0530

    fleur: update bootcontrol hal flags related to A/B bootloaders
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ AB_OTA_PARTITIONS := \
     vbmeta \
     vbmeta_system
 
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
