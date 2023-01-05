6f7d50d08d0ff08b3a48aaee3d7aba701107a71f
aghora7 <ukikieran@gmail.com>
2023-01-05 22:38:54 +0530

    fleur: build hidl variant
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -77,6 +77,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
+    
 #ImsInit
 PRODUCT_PACKAGES += \
     ImsInit
